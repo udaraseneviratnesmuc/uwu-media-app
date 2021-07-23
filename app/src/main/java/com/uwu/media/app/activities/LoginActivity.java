93e5516545d70202791f7792da6190ab3fba33e0
Udara Seneviratne <udara.seneviratne@hsenidmobile.com>
2021-07-23 16:29:10 +0530

    load Login activity after SplashActivity

---
+++ app/src/main/java/com/uwu/media/app/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.uwu.media.app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uwu.media.app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/uwu/media/app/activities/SplashActivity.java
+++ app/src/main/java/com/uwu/media/app/activities/SplashActivity.java
@@ -2,7 +2,9 @@ package com.uwu.media.app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.uwu.media.app.R;
 
@@ -12,5 +14,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
     }
 }
